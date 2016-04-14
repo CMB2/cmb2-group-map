1d7e96efe965c06c9d4b6456057a078f07e50c72
Justin Sternberg <justin@dsgnwrks.pro>
2016-04-14 00:18:04 -0400

    Add more docblocks

--- lib/init.php
+++ lib/init.php
@@ -8,12 +8,45 @@
  */
 class CMB2_Group_Map {
 
+	/**
+	 * Library version
+	 */
 	const VERSION = CMB2_GROUP_POST_MAP_VERSION;
+
+	/**
+	 * CMB2_Group_Map instance
+	 *
+	 * @var null
+	 */
 	protected static $single_instance = null;
+
+	/**
+	 * Allowed destination object types.
+	 * (only post is fully supported right now)
+	 *
+	 * @var array
+	 */
 	protected $allowed_object_types = array( 'post', 'user', 'comment', 'term' );
+
+	/**
+	 * Array of group field config arrays
+	 *
+	 * @var array
+	 */
 	protected $group_fields = array();
+
+	/**
+	 * CMB2_Field object
+	 *
+	 * @var null
+	 */
 	protected static $current_field = null;
 
+	/**
+	 * Native post fields
+	 *
+	 * @var array
+	 */
 	public static $post_fields = array(
 		'ID'                    => '',
 		'post_author'           => '',
@@ -41,6 +74,11 @@ class CMB2_Group_Map {
 		'meta_input'            => '',
 	);
 
+	/**
+	 * Native user fields
+	 *
+	 * @var array
+	 */
 	public static $user_fields = array(
 		'ID'                   => '',
 		'user_pass'            => '',
@@ -62,6 +100,11 @@ class CMB2_Group_Map {
 		'role'                 => '',
 	);
 
+	/**
+	 * Native comment fields
+	 *
+	 * @var array
+	 */
 	public static $comment_fields = array(
 		'comment_agent'        => '',
 		'comment_approved'     => '',
@@ -80,6 +123,11 @@ class CMB2_Group_Map {
 		'user_id'              => '',
 	);
 
+	/**
+	 * Native term fields
+	 *
+	 * @var array
+	 */
 	public static $term_fields = array(
 		'term'        => '',
 		'taxonomy'    => '',
@@ -89,6 +137,11 @@ class CMB2_Group_Map {
 		'slug'        => '',
 	);
 
+	/**
+	 * Library strings, filtered through cmb2_group_map_strings for translation.
+	 *
+	 * @var array
+	 */
 	public static $strings = array(
 		'missing_required' => 'Missing required data.',
 		'missing_nonce'    => 'Missing required validation nonce or failed nonce validation.',
@@ -110,6 +163,11 @@ class CMB2_Group_Map {
 		return self::$single_instance;
 	}
 
+	/**
+	 * Constructor.
+	 *
+	 * @since 0.1.0
+	 */
 	protected function __construct() {
 		add_action( 'cmb2_after_init', array( $this, 'setup_mapped_group_fields' ) );
 		add_action( 'cmb2_group_map_updated', array( $this, 'map_to_original_object' ), 10, 3 );
@@ -117,10 +175,19 @@ class CMB2_Group_Map {
 		add_action( 'wp_ajax_cmb2_group_map_delete_item', array( $this, 'ajax_delete_item' ) );
 
 		// Filter is removed by CMB2_Group_Map_Get::override_term_get(), if there is a value.
-		add_filter( 'get_the_terms', array( __CLASS__, 'override_term_get' ), 11, 3 );
+		add_filter( 'get_the_terms', array( __CLASS__, 'override_term_get' ), 11 );
 	}
 
-	public static function override_term_get( $terms, $object_id, $taxonomy ) {
+	/**
+	 * Override term-gettting when these field groups are rendering.
+	 *
+	 * @since  0.1.0
+	 *
+	 * @param  array  $terms Array of term values
+	 *
+	 * @return array         Array of term values (or empty array)
+	 */
+	public static function override_term_get( $terms ) {
 
 		/*
 		 * If we're rendering the map group
@@ -136,13 +203,18 @@ class CMB2_Group_Map {
 		return $terms;
 	}
 
+	/**
+	 * Get all instances of group fields for mapping, and initiate them.
+	 *
+	 * @since  0.1.0
+	 */
 	public function setup_mapped_group_fields() {
 
 		/**
 		 * Library's strings made available for translation.
 		 *
 		 * function cmb2_group_map_strings_i18n( $strings ) {
-		 * 	$strings['findtxt'] = __( 'Find %s', 'your-textdomain' );
+		 * 	$strings['missing_required'] = __( 'Missing required data.', 'your-textdomain' );
 		 *  	return $strings;
 		 * }
 		 * add_filter( 'cmb2_group_map_strings', 'cmb2_group_map_strings_i18n' );
@@ -168,10 +240,18 @@ class CMB2_Group_Map {
 		}
 	}
 
+	/**
+	 * Initiate the mapping field group.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param CMB2  $cmb   CMB2 instance.
+	 * @param array $field Group field config array.
+	 */
 	protected function setup_mapped_group_field( CMB2 $cmb, array $field ) {
 		$field = $this->set_object_type( $cmb, $field );
 
-		$this->set_after_group_js_hook( $cmb, $field );
+		$this->set_before_after_group_hooks( $cmb, $field );
 
 		$cmb->update_field_property( $field['id'], 'original_object_types', $cmb->prop( 'object_types' ) );
 
@@ -200,6 +280,14 @@ class CMB2_Group_Map {
 		$this->group_fields[ $cmb->cmb_id ][ $field['id'] ] = $field;
 	}
 
+	/**
+	 * Get/Set the object_type_map param.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param CMB2  $cmb   CMB2 instance.
+	 * @param array $field Group field config array.
+	 */
 	protected function set_object_type( CMB2 $cmb, array $field ) {
 		// Set object type
 		if ( ! isset( $field['object_type_map'] ) || ! in_array( $field['object_type_map'], $this->allowed_object_types, 1 ) ) {
@@ -215,7 +303,16 @@ class CMB2_Group_Map {
 		return $field;
 	}
 
-	protected function set_after_group_js_hook( CMB2 $cmb, array $field ) {
+	/**
+	 * Set the before/after group callbacks and cache/store any existing callbacks,
+	 * to keep from stomping them.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param CMB2  $cmb   CMB2 instance.
+	 * @param array $field Group field config array.
+	 */
+	protected function set_before_after_group_hooks( CMB2 $cmb, array $field ) {
 		// Let's be sure not to stomp out any existing before_group/after_group parameters.
 		if ( isset( $field['before_group'] ) ) {
 			// Store them to another field property
@@ -231,7 +328,15 @@ class CMB2_Group_Map {
 		$cmb->update_field_property( $field['id'], 'before_group', array( $this, 'before_group' ) );
 	}
 
-	public function before_group( $args, $field ) {
+	/**
+	 * Called before the mapping grouped field render begins.
+	 *
+	 * @since  0.1.0
+	 *
+	 * @param array      $args  Array of field arguments
+	 * @param CMB2_Field $field Field object.
+	 */
+	public function before_group( $args, CMB2_Field $field ) {
 		// When the field starts rendering (now), store the current field object as property.
 		self::$current_field = $field;
 
@@ -243,7 +348,15 @@ class CMB2_Group_Map {
 		echo '<div class="cmb2-group-map-group" data-nonce="'. wp_create_nonce( $field->id(), $field->id() ) .'" data-groupID="'. $field->id() .'">';
 	}
 
-	public function after_group( $args, $field ) {
+	/**
+	 * Called after the mapping grouped field render begins.
+	 *
+	 * @since  0.1.0
+	 *
+	 * @param array      $args  Array of field arguments
+	 * @param CMB2_Field $field Field object.
+	 */
+	public function after_group( $args, CMB2_Field $field ) {
 		// Check for stored 'after_group' parameter, and run that now.
 		if ( $field->args( 'cmb2_group_map_after_group' ) ) {
 			$field->peform_param_callback( 'cmb2_group_map_after_group' );
@@ -258,6 +371,15 @@ class CMB2_Group_Map {
 		add_filter( 'cmb2_script_dependencies', array( $this, 'register_js' ) );
 	}
 
+	/**
+	 * Hooked into cmb2_script_dependencies, adds the mapped field style/js dependencies.
+	 *
+	 * @since  0.1.0
+	 *
+	 * @param  array $dependencies Array of script dependencies
+	 *
+	 * @return array $dependencies Modified array of script dependencies
+	 */
 	public function register_js( $dependencies ) {
 		$dependencies['cmb2_group_map'] = 'cmb2_group_map';
 		$assets_url = $this->get_url_from_dir( CMB2_GROUP_POST_MAP_DIR ) . 'lib/assets/';
@@ -285,6 +407,15 @@ class CMB2_Group_Map {
 		return $dependencies;
 	}
 
+	/**
+	 * Gets a URL from a provided directory.
+	 *
+	 * @since  0.1.0
+	 *
+	 * @param  string $dir Directory path to convert.
+	 *
+	 * @return string      Converted URL.
+	 */
 	public function get_url_from_dir( $dir ) {
 		if ( 'WIN' === strtoupper( substr( PHP_OS, 0, 3 ) ) ) {
 			// Windows
@@ -303,15 +434,15 @@ class CMB2_Group_Map {
 		return set_url_scheme( $url );
 	}
 
-	public function post_types( $field ) {
-		if ( ! isset( $field['post_type'] ) ) {
-			$field['post_type'] = get_post_types( array( 'public' => true ) );
-			unset( $field['post_type']['attachment'] );
-		}
-
-		return $field['post_type'];
-	}
-
+	/**
+	 * Get the primary object key for the object type.
+	 *
+	 * @since  0.1.0
+	 *
+	 * @param  string $object_type Object type
+	 *
+	 * @return string              Object ID key.
+	 */
 	public static function object_id_key( $object_type ) {
 		switch ( $object_type ) {
 			case 'comment':
@@ -326,12 +457,30 @@ class CMB2_Group_Map {
 		}
 	}
 
+	/**
+	 * Hooks in the setting/getting overrides for this group field.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $field_id The group field ID.
+	 */
 	protected function hook_cmb2_overrides( $field_id ) {
 		add_filter( "cmb2_override_{$field_id}_meta_save", array( $this, 'do_save' ), 10, 4 );
 		add_filter( "cmb2_override_{$field_id}_meta_value", array( $this, 'do_get' ), 10, 4 );
-		add_filter( "cmb2_override_{$field_id}_meta_remove", array( $this, 'do_remove' ), 10, 4 );
 	}
 
+	/**
+	 * The save override
+	 *
+	 * @since  0.1.0
+	 *
+	 * @param  [type]  $override    [description]
+	 * @param  [type]  $a           [description]
+	 * @param  [type]  $args        [description]
+	 * @param  [type]  $field_group [description]
+	 *
+	 * @return bool                 Returns true to shortcircuit CMB2 setting.
+	 */
 	public function do_save( $override, $a, $args, $field_group ) {
 		require_once CMB2_GROUP_POST_MAP_DIR . 'lib/set.php';
 		$setter = new CMB2_Group_Map_Set( $field_group, $a['value'] );
@@ -340,6 +489,18 @@ class CMB2_Group_Map {
 		return true; // this shortcuts CMB2 save
 	}
 
+	/**
+	 * The get override
+	 *
+	 * @since  0.1.0
+	 *
+	 * @param  [type] $nooverride [description]
+	 * @param  [type] $object_id  [description]
+	 * @param  [type] $a          [description]
+	 * @param  [type] $field      [description]
+	 *
+	 * @return mixed              By not returning the passed-in value, we shortcircuit CMB2 getting.
+	 */
 	public function do_get( $nooverride, $object_id, $a, $field ) {
 		remove_filter( "cmb2_override_{$a['field_id']}_meta_value", array( $this, 'do_get' ), 10, 4 );
 
@@ -349,12 +510,17 @@ class CMB2_Group_Map {
 		return $value; // this shortcuts CMB2 get
 	}
 
-	public function do_remove( $override, $a, $args, $field ) {
-		throw new Exception( 'hey, fix this: '. __METHOD__ );
-		return true; // this shortcuts CMB2 remove
-	}
-
-	public function map_to_original_object( $updated, $original_object_id, $field ) {
+	/**
+	 * Hooked into cmb2_group_map_updated, maps the updated/saved posts
+	 * to the original parent post.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param array      $updated            Array of updated IDs
+	 * @param int        $original_object_id Parent id
+	 * @param CMB2_Field $field              Group field object.
+	 */
+	public function map_to_original_object( $updated, $original_object_id, CMB2_Field $field ) {
 		$updated    = is_array( $updated ) ? $updated : array();
 		$object_ids = array();
 
@@ -374,6 +540,11 @@ class CMB2_Group_Map {
 		}
 	}
 
+	/**
+	 * Ajax handler for getting group data.
+	 *
+	 * @since  0.1.0
+	 */
 	public function get_ajax_input_data() {
 		require_once CMB2_GROUP_POST_MAP_DIR . 'lib/ajax.php';
 
@@ -385,6 +556,11 @@ class CMB2_Group_Map {
 		}
 	}
 
+	/**
+	 * Ajax handler for deleting associated posts.
+	 *
+	 * @since  0.1.0
+	 */
 	public function ajax_delete_item() {
 		require_once CMB2_GROUP_POST_MAP_DIR . 'lib/ajax.php';
 
@@ -396,10 +572,25 @@ class CMB2_Group_Map {
 		}
 	}
 
+	/**
+	 * Helper method to determine if Lib is in the middle of
+	 * rendering a group mapping field.
+	 *
+	 * @since  0.1.0
+	 *
+	 * @return boolean True if rendering.
+	 */
 	public static function is_rendering() {
 		return (bool) self::$current_field;
 	}
 
+	/**
+	 * Return current field.
+	 *
+	 * @since  0.1.0
+	 *
+	 * @return CMB2_Field|null If in the middle of rendering, will be the current group field object.
+	 */
 	public static function get_current_field() {
 		return self::$current_field;
 	}
